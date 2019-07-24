b54105a84cb987cf0e5cce3b7c13d14cfbfc149b
buttonj <buttonj@HHS384881>
2019-07-24 12:27:57 -0700

    Added link to github

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,6 +3,12 @@ import React from 'react'
 export function Header() {
   return (
     <header>
+      <a
+        className='text-green-600 font-bold underline '
+        href='https://github.com/muadjb/ReactPeopleFilter'
+      >
+        Github
+      </a>
       <h1 className='text-4xl text-center'>People Filter</h1>
     </header>
   )
